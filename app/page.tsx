f6a2db7bb24ba4d5f4b0766be1de28f64c2eced0
A-shuk <a77shukla@gmail.com>
2026-01-12 15:53:42 +1000

    finished companions popular cards on home pg

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,47 @@
+import CompanionCard from '@/components/CompanionCard'
+import CompanionList from '@/components/CompanionsList'
+import CTA from '@/components/CTA'
 import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const Page = () => {
   return (
-    <div>
-      <h1 className='text-2xl underline'>Welcome to my SaaS app</h1>
-      <Button>Click me</Button>
-    </div>
+    <main>
+      <h1 className='text-2xl'>Popular Companions</h1>
+      <section className='home-section'>
+        <CompanionCard
+        id = "123"
+        name = "Neura the Brainy Explorer"
+        topic = "Neural Network of the Brain"
+        subject = "Science"
+        duration = {45}
+        color = "#ffda6e"
+        
+        />
+        <CompanionCard
+         id = "456"
+         name = "Countsy the Number Wizard"
+         topic = "Derivatives & Integrals"
+         subject = "Maths"
+         duration = {30}
+         color = "#e5d0ff"
+         
+        />
+        <CompanionCard
+         id = "789"
+         name = "Verba the Vocabularly Builder"
+         topic = "English Literature"
+         subject = "Language"
+         duration = {30}
+         color = "#BDE7FF"
+        />
+      </section>
+      <section className = "home-section">
+        <CompanionList/>
+        <CTA/>
+      </section>
+      
+    </main>
   )
 }
 
---
+++ components/CompanionCard.tsx
@@ -0,0 +1,55 @@
+import Link from "next/link"
+// interface for CompanionCardProps on dashboard pg
+
+interface CompanionCardProps {
+    id: string
+    name: string
+    topic: string
+    subject: string
+    duration: number
+    color: string
+}
+
+/**
+ * A card component representing a companion.
+ *
+ * @param {CompanionCardProps} props - The component props.
+ * @param {string} props.id - The ID of the companion.
+ * @param {string} props.name - The name of the companion.
+ * @param {string} props.topic - The topic of the companion.
+ * @param {string} props.subject - The subject of the companion.
+ * @param {number} props.duration - The duration of the companion.
+ * @param {string} props.color - The color of the companion.
+ */
+const CompanionCard = ({id, name, topic, subject, duration, color}: CompanionCardProps) => {
+    //ui of the card (colour and style) through article
+    return (
+        <article className="companion-card" style = {{backgroundColor: color}}>
+            {/* pass in card content (subject, bookmark icon) through div*/}
+            <div className="flex justify-between items-center">
+                <div className="subject-badge">{subject}</div>
+                {/* pass in bookmark icon through button*/}
+                <button className="companion-bookmark">
+                    <img src = "/icons/bookmark.svg" alt = "bookmark" width = {12.5} height = {15} />
+
+                </button>
+
+             </div>
+             {/* pass in card content (name, topic, duration) through div*/}
+             <h2 className="text-2xl font-bold">{name}</h2>
+             <p className="text-sm">{topic}</p>
+             <div className="flex items-center gap-2">
+                <img src = "/icons/clock.svg" alt = "clock" width = {13.5} height = {13.5} />
+                <p className="text-sm">{duration} minutes </p>
+             </div>
+             {/* pass in launch lesson button through link*/}
+             <Link href = {'/compaions/${id}'} className = "w-full">
+                <button className="btn-primary w-full justify-center">Launch Lesson</button>
+                
+             </Link>
+        </article>
+        
+    )
+}
+
+export default CompanionCard
