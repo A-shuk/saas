2c0265da63e5cffe17c98e3a25e4efca35a0301a
A-shuk <a77shukla@gmail.com>
2026-01-19 17:47:00 +1000

    companions display on companionlib pg

--- app/companions/page.tsx
+++ app/companions/page.tsx
@@ -1,7 +1,39 @@
+import { getAllCompanions } from '@/lib/actions/companion.actions'
+import CompanionCard from '@/components/CompanionCard';
+import { getSubjectColor } from '@/lib/utils';
 
-const CompanionsLibrary = () => {
+
+/**
+ * A page component to display a list of companions based on search parameters.
+ * @param {SearchParams} searchParams - The search parameters.
+ * @returns {JSX.Element} - A JSX element representing the page.
+ */
+const CompanionsLibrary = async ({searchParams}: SearchParams) => {
+  //fetch companions
+
+  const filters = await searchParams;
+  const subject = filters.subject ? filters.subject : '';
+  const topic = filters.topic ? filters.topic : '';
+  const companions = await getAllCompanions({subject, topic});
+  console.log(companions);
+
   return (
-    <div>CompanionsLibrary</div>
+    // display companions on pg
+    <main>
+      <section className='flex justify-between gap-4 max-sm:flex-col'>
+        <h1>Companion Library</h1>
+        <div className='flex gap-4'>Filters</div>
+
+      </section>
+      <section className='companions-grid'>
+        {/* map over the companions array and render a card for each companion*/}
+        {companions.map((companion) => (
+          <CompanionCard key={companion.id} {...companion} color = {getSubjectColor(companion.subject)} />
+          
+        ))}
+
+      </section>
+    </main>
   )
 }
 
--- lib/actions/companion.actions.ts
+++ lib/actions/companion.actions.ts
@@ -1,6 +1,7 @@
 'use server';
 import { auth } from '@clerk/nextjs/server';
 import { createSupabaseClient } from '../supabase';
+
 /**
  * Creates a new learning companion with the given form data.
  * @param formData The form data of the companion to be created.
@@ -22,4 +23,43 @@ export const createCompanion = async(formData: CreateCompanion) => {
     }
 
     return data[0];
+}
+
+//fetch companions to companion library pg action:
+
+export const getAllCompanions = async({limit = 10, page = 1, subject, topic}: GetAllCompanions ) => {
+    const supabase = createSupabaseClient(); // create supabase client (fetch from supabase)
+
+    let query = supabase.from('companions').select();
+    // subject AND topic
+  if (subject && topic) {
+    query = query
+      .ilike('subject', `%${subject}%`)
+      .or(`topic.ilike.%${topic}%,name.ilike.%${topic}%`);
+  }
+
+  // subject only
+  else if (subject) {
+    query = query.ilike('subject', `%${subject}%`);
+  }
+
+  // topic only
+  else if (topic) {
+    query = query.or(`topic.ilike.%${topic}%,name.ilike.%${topic}%`);
+  }
+
+  
+
+    query = query.range((page - 1) * limit, page * limit - 1);
+    //fetch data from db
+
+    const {data: companions, error} = await query;
+
+    if (error || !companions) {
+        throw new Error(error?.message || 'Failed to fetch companions');
+    }
+
+    return companions; //return companions
+
+
 }
